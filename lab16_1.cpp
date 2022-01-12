39a9f0d3d64e5d5d0a44ef22bd9fbab9b5bde06f
Zeronos <boss16805@gmail.com>
2022-01-12 18:57:27 +0700

    lab16_1 completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,26 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5;
+    char b = 'A';
+    char &c = b;
+    
+    int *x = &a;
+    char &y = b;
+    int **z = &x;
+    
+    void *m = &b;
+    void *n = &c;
+    cout << a << " " << b << " " << c << " " << x << " " << m << " " << z << " " << "\n";
+    cout << &a << " " << m << " " << n << " " << &x << " " << &m << " " << z << " " << "\n";
+    c = 'F';
+    cout << a << " " << b << " " << c << " " << x << " " << m << " " << z << " " << "\n";
+    y = 'W';
+    cout << a << " " << b << " " << c << " " << x << " " << m << " " << z << " " << "\n";
+    **z = 6;
+    cout << a << " " << b << " " << c << " " << x << " " << m << " " << z << " " << "\n";
+    **z = 7;
+    cout << a << " " << b << " " << c << " " << x << " " << m << " " << z;
 	return 0;
 }
